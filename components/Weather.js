89877418bab43c452f1999792ab9a3761b37772d
6210110459 <6210110459@psu.ac.th>
2021-07-18 17:37:31 +0700

    part5

--- components/Weather.js
+++ components/Weather.js
@@ -7,20 +7,20 @@ export default function Weather(props){
     useEffect(() => {
         console.log(`fetching data with zipCode = ${props.zipCode}`)
         if (props.zipCode) {
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=57b8acd28c647867c435f2b2eca7cfe7`)
-        .then((response) => response.json())
-        .then((json) => {
-        setForecastInfo({
-        main: json.weather[0].main,
-        description: json.weather[0].description,
-        temp: json.main.temp
-        });
-        })
-        .catch((error) => {
-        console.warn(error);
-        });
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=57b8acd28c647867c435f2b2eca7cfe7`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
         }
-        }, [props.zipCode])
+    }, [props.zipCode])
        
     const [forecastInfo, setForecastInfo] = useState({
         main: 'main',
